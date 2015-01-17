c8487e33252e4dcedcf47e30387f66604535651b
Ubuntu <ubuntu@ip-10-126-50-143.ap-northeast-1.compute.internal>
2015-01-17 09:20:33 +0000

    fixed support cudnn install command

--- install_nvidia_cuda.sh
+++ install_nvidia_cuda.sh
@@ -1,11 +1,21 @@
 #This script should be used on Amazon EC2 g2.2xlarge server with ubuntu 14.04 trusty version
+    sudo apt-get -y install build-essential
     sudo apt-get update
     sudo apt-get -y dist-upgrade
     sudo apt-get install -y linux-image-generic
+    sudo apt-get install -y linux-generic
+
+    #this version will support Nvidia GTX 980 & 970
     wget http://developer.download.nvidia.com/compute/cuda/6_5/rel/installers/cuda-repo-ubuntu1404-6-5-prod_6.5-19_amd64.deb
     sudo dpkg -i cuda-repo-ubuntu1404-6-5-prod_6.5-19_amd64.deb
     sudo apt-get update
     sudo apt-get install -y cuda
-    #sometimes this will fail => add manually
-    echo -e "\nexport PATH=/usr/local/cuda-6.5/bin:$PATH\n\nexport LD_LIBRARY_PATH=/usr/local/cuda-6.5/lib64:$LD_LIBRARY_PATH" >> .bashrc
+    echo -e "\nexport PATH=/usr/local/cuda/bin:\$PATH\n\nexport LD_LIBRARY_PATH=/usr/local/cuda/lib64:\$LD_LIBRARY_PATH" >> .bashrc && source ~/.bashrc
+
+    #this install cudnn, you need to manually download the cudnn R1 library, and put it to your home directory from Nvidia
+    cd ~
+    tar -xvf cudnn-6.5-linux-R1.tar
+    sudo cp cudnn-6.5-linux-R1/*.h /usr/local/cuda/include
+    sudo cp cudnn-6.5-linux-R1/*.so* /usr/local/cuda/lib64
+
     sudo reboot
