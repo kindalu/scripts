c73a02c8708a2312baa53713f269e101ba308b88
WANGCHOU LU <awan@zillians>
2015-01-18 10:57:37 +0800

    install fbcunn

--- install_fbcunn.sh
+++ install_fbcunn.sh
@@ -9,18 +9,18 @@ sudo apt-get update
 sudo apt-get install -y cuda
 echo "export PATH=/usr/local/cuda/bin/:\$PATH; export LD_LIBRARY_PATH=/usr/local/cuda/lib64/:\$LD_LIBRARY_PATH; " >>~/.bashrc && source ~/.bashrc
 
+################## Instructions ################################
 #Now you need to restart your computer
-
 #Install Cudnn, need to download the libray from Nvidia manually
 cd ~
 tar -xvf cudnn-6.5-linux-R1.tar
 sudo cp cudnn-6.5-linux-R1/*.h /usr/local/cuda/include
 sudo cp cudnn-6.5-linux-R1/*.so* /usr/local/cuda/lib64
 
-
 #Install Torch Dependencies
 curl -sk https://raw.githubusercontent.com/torch/ezinstall/master/install-deps | bash
 
+#Before install this you need to make sure there is no previous installed torch
 #Install Torch in a local folder
 cd ~/
 git clone https://github.com/soumith/torch-distro.git
@@ -32,9 +32,9 @@ echo "export PATH=~/torch-distro/install/bin:\$PATH; export LD_LIBRARY_PATH=~/to
 curl -sk https://raw.githubusercontent.com/soumith/fblualib/master/install_all.sh | bash
 
 #Install fbcunn
+mkdir ~/tmp
+cd ~/tmp
 git clone https://github.com/torch/nn && cd nn && git checkout getParamsByDevice && luarocks make rocks/nn-scm-1.rockspec
+cd ~/tmp
 git clone https://github.com/facebook/fbcunn.git
 cd fbcunn && luarocks make rocks/fbcunn-scm-1.rockspec # go get a coffee
-
-#asked to install after running
-sudo apt-get install -y GraphicsMagick
